40531f1e94b7765bdaa20af77fc857d0edd76004
xiaoyueyue165 <1656800216@qq.com>
2019-07-30 13:31:18 +0800

    feat: 分页

--- src/pages/List.vue
+++ src/pages/List.vue
@@ -16,6 +16,10 @@ export default {
   data() {
     return {
       postList: [],
+      loaded: false,
+      page: 1,
+      number: 30,
+      total_count: '',
     };
   },
   watch: {
@@ -23,16 +27,49 @@ export default {
       this.getIssueList();
     },
   },
+  mounted() {
+    this.handleScroll();
+  },
   methods: {
     getIssueList() {
       this.$q.loading.show({ delay: 250 });
-      let url = `/search/issues?q=+repo:${this.$store.getters.repository}+state:open`;
+      let url = `/search/issues?q=+repo:${this.$store.getters.repository}+state:open&page=${this.page}&per_page=${this.number}`;
       if (this.$route.query.label) {
         url += `+label:${this.$route.query.label}`;
       }
       axiosInstance.get(url)
         .then((res) => {
           this.$set(this, 'postList', res.data.items);
+          this.$set(this, 'total_count', res.data.total_count);
+          this.$set(this, 'loaded', true);
+          this.$q.loading.hide();
+        });
+    },
+    handleScroll() {
+      const that = this;
+      window.onscroll = () => {
+        // 距离底部200px时加载一次
+        const bottomOfWindow =
+          document.documentElement.offsetHeight -
+            document.documentElement.scrollTop -
+            window.innerHeight <=
+          200;
+        if (bottomOfWindow && that.loaded && that.total_count > that.postList.length) {
+          that.loadMore(that.page + 1, that.number);
+        }
+      };
+    },
+    loadMore(page, num) {
+      let url = `/search/issues?q=+repo:${this.$store.getters.repository}+state:open&page=${page}&per_page=${num}`;
+      if (this.$route.query.label) {
+        url += `+label:${this.$route.query.label}`;
+      }
+      this.$set(this, 'loaded', false);
+      axiosInstance.get(url)
+        .then((res) => {
+          this.$set(this, 'postList', this.postList.concat(res.data.items));
+          this.$set(this, 'total_count', res.data.total_count);
+          this.$set(this, 'loaded', true);
           this.$q.loading.hide();
         });
     },
