181e2a026271fb7e06f3ccbbefed7fde14293c7c
ttop5 <ttop5@qq.com>
2019-09-03 17:43:07 +0800

    perf: 改了个名字

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export default {
   username: 'ttop5', // GitHub 用户名
   repository: 'blog', // 记录 issue 的仓库名
   accessToken: 'MGNhMTQ3YTRlMGQ0NGFkM2JjZTdmMTI5MTQzYWFkY2ZjMWQ0NmMyNg==', // 经过 base64 加密后的 GitHub Token
-  blogName: 'ISSUE BLOG', // 给你的博客取个名字
+  blogName: 'TTOP5', // 给你的博客取个名字
   /**
    * 定制左侧菜单链接部分
    * 格式：
